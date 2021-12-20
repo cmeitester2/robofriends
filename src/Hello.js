3ceee2f6df62d864194c59f416ec7cafd03a1eda
Sanjay <sgupt9999@gmail.com>
2021-12-19 20:09:40 -0600

    Section-310

--- src/Hello.js
+++ src/Hello.js
@@ -12,4 +12,14 @@ class Hello extends Component {
     };
 };
 
+/* const Hello = (props) => {
+            return (
+            <div className='f1 tc'>
+                <h1>Hello</h1>
+                <p>{props.greeting}</p>
+            </div>
+        )
+}
+*/
+
 export default Hello;
