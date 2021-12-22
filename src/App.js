b3576d995fd8d9edb913c4f139d67a0a8e60da63
Sanjay <sgupt9999@gmail.com>
2021-12-21 19:00:49 -0600

    Section-316. Reading dummy robots data via an API, rather than a from a file

--- src/App.js
+++ src/App.js
@@ -7,16 +7,42 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
     };
+
+    /*
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                this.setState({ robots: users })
+            });
+    }
+    */
+
+    async componentDidMount() {
+        const users = await fetch('https://jsonplaceholder.typicode.com/users')
+        const usersJson = await users.json();
+        /* Code to delay execution by 7 seconds 
+        var start = new Date().getTime();
+        var end = start;
+        while (end < start + 7000) {
+            end = new Date().getTime();
+        }
+        */
+
+        this.setState({ robots: usersJson })
+    }
 
     onSearchChange = (event) => { //Need to make this an arrow function so this will refer to where this function was created
         // otherwise this will refer to the event
         // the other option is onSearchChange(event) = 
         this.setState({ searchField: event.target.value });
-        console.log(event.target.value);
+        //console.log(event.target.value);
 
     };
 
@@ -24,14 +50,18 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         });
-        console.log(filteredRobots);
-        return (
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+        //console.log(filteredRobots);
+        if (this.state.robots.length === 0) {
+            return <h1>Loading....</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            )
+        }
     }
 };
 
